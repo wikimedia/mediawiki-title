2faead95b9298895bbcafd03c745b64701b43964
Petr Pchelko <ppchelko@wikimedia.org>
2016-02-24 18:03:21 -0800

    Added IP normalizations for user pages

--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ var assert = require('assert');
 var Normalizer = require('../lib/index');
 
 // Run jshint as part of normal testing
-//require('mocha-jshint')();
+require('mocha-jshint')();
 // Run jscs as part of normal testing
-//require('mocha-jscs')();
+require('mocha-jscs')();
 
 var normalizer = new Normalizer({
     apiURI: function (domain) {
@@ -26,16 +26,36 @@ describe('General', function() {
     });
     it('should throw error for non-string domain', function() {
         try {
-            normalizer.normalize('Test_Page', 1);
+            normalizer.normalize('a', 2);
             throw new Error('Should throw error!');
         } catch (e) {
             assert.deepEqual(e.constructor, TypeError);
             assert.deepEqual(e.message, 'Invalid type of domain parameter. Must be a string');
         }
     });
+    it('should throw error for undefined options', function() {
+        try {
+            new Normalizer();
+            throw new Error('Should throw error!');
+        } catch (e) {
+            assert.deepEqual(e.constructor, TypeError);
+            assert.deepEqual(e.message, 'Invalid options for Normalizer constructor');
+        }
+    });
+    it('should throw error for invalid apiURI', function() {
+        try {
+            new Normalizer({
+                apiURI: 'test'
+            });
+            throw new Error('Should throw error!');
+        } catch (e) {
+            assert.deepEqual(e.constructor, TypeError);
+            assert.deepEqual(e.message, 'Invalid options for Normalizer constructor');
+        }
+    });
 });
 
-describe('Title validation', function () {
+describe('Validation', function () {
     var invalidTitles = [['', 'title-invalid-empty'],
         [':', 'title-invalid-empty'],
         ['__  __', 'title-invalid-empty'],
@@ -74,7 +94,8 @@ describe('Title validation', function () {
         ['A ~~~~ Signature', 'title-invalid-magic-tilde'],
         ['A ~~~~~ Timestamp', 'title-invalid-magic-tilde'],
         // Length
-        // TODO [('x', 256), 'title-invalid-too-long'],
+        [ new Array(257).join('x'), 'title-invalid-too-long' ],
+        [ 'Special:' + new Array(514).join('x'), 'title-invalid-too-long' ],
         // Namespace prefix without actual title
         ['Talk:', 'title-invalid-empty'],
         ['Talk:#', 'title-invalid-empty'],
@@ -82,7 +103,12 @@ describe('Title validation', function () {
         ['Category: #bar', 'title-invalid-empty']];
 
     invalidTitles.forEach(function(testCase) {
-        it('should throw ' + testCase[1] + ' error for ' + testCase[0], function() {
+        var name = testCase[0];
+        if (name.length > 20) {
+            name = testCase[0].substr(0, 20) + '...'
+        }
+
+        it('should throw ' + testCase[1] + ' error for ' + name, function() {
             return normalizer.normalize(testCase[0], 'en.wikipedia.org')
             .then(function () {
                 throw new Error('Error should be thrown');
@@ -108,16 +134,56 @@ describe('Title validation', function () {
         [ 'Foo/.../Sandbox' ],
         [ 'Sandbox/...' ],
         [ 'A~~' ],
-        [ ':A' ]
+        [ ':A' ],
         // Length is 256 total, but only title part matters
-        // TODO [ 'Category:' . str_repeat( 'x', 248 ) ],
-        // TODO [ str_repeat( 'x', 252 ) ],
+        [ 'Category:' + new Array(248).join('x') ],
+        // Special pages can have longer titles
+        [ 'Special:' + new Array(500).join('x') ],
+        [ new Array(252).join('x') ]
     ];
 
     validTitles.forEach(function(title) {
-        it(title[0] + ' should be valid', function() {
+        var name = title[0];
+        if (name.length > 20) {
+            name = title[0].substr(0, 20) + '...'
+        }
+
+        it(name + ' should be valid', function() {
             return normalizer.normalize(title[0], 'en.wikipedia.org')
         });
     });
 });
 
+describe('Normalization', function() {
+    var testCases = [
+        [ 'en.wikipedia.org', 'Test', 'Test'],
+        [ 'en.wikipedia.org', 'Foo:bar', 'Foo:bar'],
+        [ 'en.wikipedia.org', 'Talk: foo', 'Talk:Foo'],
+        [ 'en.wikipedia.org', 'int:eger', 'Int:eger'],
+        // User IP sanitizations
+        [ 'en.wikipedia.org', 'User:::1', 'User:0:0:0:0:0:0:0:1'],
+        [ 'en.wikipedia.org', 'User:127.000.000.001', 'User:127.0.0.1'],
+        [ 'en.wikipedia.org', 'User:0.0.0.0', 'User:0.0.0.0' ],
+        [ 'en.wikipedia.org', 'User:00.00.00.00', 'User:0.0.0.0' ],
+        [ 'en.wikipedia.org', 'User:000.000.000.000', 'User:0.0.0.0'],
+        [ 'en.wikipedia.org', 'User:141.000.011.253', 'User:141.0.11.253' ],
+        [ 'en.wikipedia.org', 'User: 1.2.4.5', 'User:1.2.4.5' ],
+        [ 'en.wikipedia.org', 'User:01.02.04.05', 'User:1.2.4.5' ],
+        [ 'en.wikipedia.org', 'User:001.002.004.005', 'User:1.2.4.5' ],
+        [ 'en.wikipedia.org', 'User:010.0.000.1', 'User:10.0.0.1' ],
+        [ 'en.wikipedia.org', 'User:080.072.250.04', 'User:80.72.250.4' ],
+        [ 'en.wikipedia.org', 'User:Foo.1000.00', 'User:Foo.1000.00' ],
+        [ 'en.wikipedia.org', 'User:Bar.01', 'User:Bar.01' ],
+        [ 'en.wikipedia.org', 'User:Bar.010', 'User:Bar.010' ]
+
+    ];
+
+    testCases.forEach(function (test) {
+        it('For ' + test[0] + ' should normalize ' + test[1] + ' to ' + test[2], function() {
+            return normalizer.normalize(test[1], test[0])
+            .then(function(res) {
+                assert.deepEqual(res, test[2]);
+            });
+        });
+    });
+});
